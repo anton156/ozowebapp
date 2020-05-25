b1ab66fee71b133bb8d3bb653cef505d4c1ee276
anton156 <User>
2020-05-25 15:08:30 +0200

    Added oprema crud

---
+++ ozowebapp/Controllers/IndexOpremaController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ozowebapp.Models;
+
+namespace ozowebapp.Controllers
+{
+    public class IndexOpremaController : Controller
+    {
+        private readonly ConnectionStringClass _context;
+
+        public IndexOpremaController(ConnectionStringClass context)
+        {
+            _context = context;
+        }
+
+        // GET: IndexOprema
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Oprema.ToListAsync());
+        }
+
+        // GET: IndexOprema/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dodajOpremu = await _context.Oprema
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dodajOpremu == null)
+            {
+                return NotFound();
+            }
+
+            return View(dodajOpremu);
+        }
+
+        // GET: IndexOprema/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IndexOprema/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Naziv,Kolicina,Cijena,Duljina_Koristenja_u_h,Lokacija,Datum_proizvodnje,Usluga_ID,Natjecaj_ID")] DodajOpremu dodajOpremu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dodajOpremu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dodajOpremu);
+        }
+
+        // GET: IndexOprema/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dodajOpremu = await _context.Oprema.FindAsync(id);
+            if (dodajOpremu == null)
+            {
+                return NotFound();
+            }
+            return View(dodajOpremu);
+        }
+
+        // POST: IndexOprema/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Naziv,Kolicina,Cijena,Duljina_Koristenja_u_h,Lokacija,Datum_proizvodnje,Usluga_ID,Natjecaj_ID")] DodajOpremu dodajOpremu)
+        {
+            if (id != dodajOpremu.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dodajOpremu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DodajOpremuExists(dodajOpremu.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dodajOpremu);
+        }
+
+        // GET: IndexOprema/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dodajOpremu = await _context.Oprema
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dodajOpremu == null)
+            {
+                return NotFound();
+            }
+
+            return View(dodajOpremu);
+        }
+
+        // POST: IndexOprema/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dodajOpremu = await _context.Oprema.FindAsync(id);
+            _context.Oprema.Remove(dodajOpremu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DodajOpremuExists(int id)
+        {
+            return _context.Oprema.Any(e => e.ID == id);
+        }
+    }
+}
--- ozowebapp/Models/DodajOpremu.cs
+++ ozowebapp/Models/DodajOpremu.cs
@@ -8,20 +8,21 @@ namespace ozowebapp.Models
 {
     public class DodajOpremu
     {
+        [Key]
         public int ID { get; set; }
         [Required(ErrorMessage="Ne smije biti prazno")]
         public string Naziv { get; set; }
         [Range(0, 99999,ErrorMessage = "Ne smije")]
         [Required(ErrorMessage = "Ne smije biti prazno")]
-        public int Kolicina { get; set; }
-        [Required(ErrorMessage = "Ne smije biti prazno")]
-        public float Cijena { get; set; }
-        [Required(ErrorMessage = "Ne smije biti prazno")]
-        public float Duljina_Koristenja_u_h { get; set; }
-        [Required(ErrorMessage = "Ne smije biti prazno")]
-        public string Lokacija { get; set; }
+        public int? Kolicina { get; set; }
         
-        public string Datum_proizvodnje { get; set; }
+        public double? Cijena { get; set; }
+        
+        public double? Duljina_Koristenja_u_h { get; set; }
+        
+        public string? Lokacija { get; set; }
+        
+        public DateTime? Datum_proizvodnje { get; set; }
         
         public int? Usluga_ID { get; set; }
         public int? Natjecaj_ID { get; set; }
