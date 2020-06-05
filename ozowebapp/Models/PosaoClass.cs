33f0810f2ddb5b78c47d6fee271367ac6d2cacec
anton156 <User>
2020-06-05 12:16:38 +0200

    added logo

---
+++ ozowebapp/Models/PosaoClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ozowebapp.Models
+{
+    public class PosaoClass
+    {
+        [Key]
+        public int PosaoClassID { get; set; }
+        public string Naziv { get; set; }
+        public double Cijena { get; set; }
+
+        public string Opis { get; set; }
+        public string Lokacija { get; set; }
+    }
+}
